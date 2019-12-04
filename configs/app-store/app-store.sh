cd87215e580fe22875217928d713cc2bbafac8ff
Sam Kasman <sk@samkasman.com>
2019-12-04 13:25:37 -0500

    Enable App Store apps

--- configs/app-store/app-store.sh
+++ configs/app-store/app-store.sh
@@ -15,11 +15,26 @@ printf "\e[0m"
 # App Store Stuff
 # --
 
-# 1295203466 Microsoft Remote Desktop (10.3.5)
-# 1063996724 Tyme 2 (2019.15)
-# 682658836 GarageBand (10.3.3)
-# 408981434 iMovie (10.1.13)
-# 409201541 Pages (8.2.1)
-# 409183694 Keynote (9.2.1)
-# 1006087419 SnippetsLab (1.9.2)
-# 409203825 Numbers (6.2.1)
+# GarageBand
+mas install 682658836
+
+# iMovie
+mas install 408981434
+
+# Keynote
+mas install 409183694
+
+# Microsoft Remote Desktop
+mas install 1295203466
+
+# Numbers
+mas install 409203825
+
+# Pages
+mas install 409201541
+
+# SnippetsLab
+mas install 1006087419
+
+# Tyme 2
+mas install 1063996724
