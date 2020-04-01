5cb8023ae384985ce702646ee7afdff4bdf3162d
Sam Kasman <sk@samkasman.com>
2020-04-01 19:25:32 -0400

    Add Ubersicht script

---
+++ configs/ubersicht/ubersicht.sh
@@ -0,0 +1,20 @@
+# --
+# Ubersicht
+# --
+
+# Init Message
+# --
+printf "\e[96m"
+echo "--"
+echo "Ubersicht"
+echo "--"
+echo ""
+printf "\e[0m"
+
+
+# Main stuff
+# --
+
+echo "${RED}Nothing to do yet.${WHITE}"
+
+echo "\\n"
