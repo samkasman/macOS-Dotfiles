9e0fd1d16cee190c39e43b437fdcd09396a872ac
Sam Kasman <sk@samkasman.com>
2020-04-01 19:15:33 -0400

    VSCode: styles

--- configs/vscode/vscode.sh
+++ configs/vscode/vscode.sh
@@ -1,6 +1,6 @@
-#
+# --
 # VSCode
-#
+# --
 
 # Init Message
 # --
@@ -15,6 +15,10 @@ printf "\e[0m"
 # Main stuff
 # --
 
+echo "${RED}Nothing to do yet.${WHITE}"
+
 # Install Settings Sync extension to pull everything from https://gist.github.com
 
 # Enable 'code' command
+
+echo "\\n"
